d9db6b9b41a8f86b6e1dc979ede005387c67635d
arupadhy <arvind.charlotte@gmail.com>
2016-04-18 17:28:59 -0400

    real world app structure

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'my-app',
+	templateUrl: 'app/app.componnet.html',
+	styleUrls: ['app/app.component.css']
+})
+export class AppComponent {
+
+	title:string = "Tennis partner finder";
+}
